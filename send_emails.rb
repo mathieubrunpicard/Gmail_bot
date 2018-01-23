451f7d01a288495b35e0af2b00421772b93a8fc0
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-23 17:01:17 +0100

    Worked issue with type message in main script

--- send_emails.rb
+++ send_emails.rb
@@ -35,8 +35,8 @@ def third_choice
     Nous apprenons l'informatique en faisant des projets concrets qui nous sont assignés tous les jours, sur lesquel nous planchons en petites équipes autonomes.
     Le projet du jour est d'envoyer des emails à nos élus locaux pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation gratuite.
 
-    Nous vous contactons pour vous parler du projet, et vous dire que vous pouvez ouvrir une cellule à #{value}, où vous pouvez former gratuitement 6 personnes (ou plus), qu'elles soient débutantes, ou confirmées.
-    Le modèle d'éducation de The Hacking Project n'a pas de limite en terme de nombre d'élèves, donc nous serions ravis de travailler avec #{value} !
+    Nous vous contactons pour vous parler du projet, et vous dire que vous pouvez ouvrir une cellule à {value}, où vous pouvez former gratuitement 6 personnes (ou plus), qu'elles soient débutantes, ou confirmées.
+    Le modèle d'éducation de The Hacking Project n'a pas de limite en terme de nombre d'élèves, donc nous serions ravis de travailler avec {value} !
     Si vous voulez avoir plus d'information sur la formation, n'hésitez pas à visiter mon blog où je décris le quotidien des étudiants : https://medium.com@mathieu.bp/ .
 
     Si vous avez des questions sur la formation, n'hésitez pas à me recontacter.
