e67f2817d75e60ae3f00dfa031572d0a1b6c9039
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-23 16:58:40 +0100

    Updated README

--- README.md
+++ README.md
@@ -6,5 +6,14 @@
   <li>Mail bot</li>
 </ol>
 
+<h3>Scrapping tool</h3>
 <p>The scrapping tool get a french department as an input (i.e. between 01 and 95, including 971, 972, 973 and 974).</p>
 <p>Then it returns a file "townhall.json" containing all the townhalls name and contact e-mail adress</p>
+<p>It also saves the data in a spreadsheet</p>
+<p>I decided to work with a json, as the execution speed is <em>higher</em> on my laptop</p>
+
+<h3>Mail bot</h3>
+
+<p>The mail bot asks you if you want to send a predifinite email.</p>
+<p>If you press Yes, then the bot will send the mail to the whole townhall of the selected department.</p>
+
--- send_emails.rb
+++ send_emails.rb
@@ -28,9 +28,30 @@ def third_choice
   puts "Do you want to send them an email? [Y/N]"
   user_input = gets.chomp
   if user_input == "Y"
-    puts "The content of the email will be the following:"
-    require_relative 'mail_content'
+    puts "The content of the email will be the following: "
+    puts "Bonjour,
+    Je m'appelle Mathieu Brun-Picard et je suis actuellement élève à une formation de code : The Hacking Project (http://thehackingproject.org/).
+    Cette formation est gratuite, ouverte à tous, sans restriction géographique, ni restriction de niveau.
+    Nous apprenons l'informatique en faisant des projets concrets qui nous sont assignés tous les jours, sur lesquel nous planchons en petites équipes autonomes.
+    Le projet du jour est d'envoyer des emails à nos élus locaux pour qu'ils nous aident à faire de The Hacking Project un nouveau format d'éducation gratuite.
 
+    Nous vous contactons pour vous parler du projet, et vous dire que vous pouvez ouvrir une cellule à #{value}, où vous pouvez former gratuitement 6 personnes (ou plus), qu'elles soient débutantes, ou confirmées.
+    Le modèle d'éducation de The Hacking Project n'a pas de limite en terme de nombre d'élèves, donc nous serions ravis de travailler avec #{value} !
+    Si vous voulez avoir plus d'information sur la formation, n'hésitez pas à visiter mon blog où je décris le quotidien des étudiants : https://medium.com@mathieu.bp/ .
+
+    Si vous avez des questions sur la formation, n'hésitez pas à me recontacter.
+    Bien à vous,
+
+    Mathieu Brun-Picard"
+
+    puts "Press Yes to continue [Y/N]"
+    user_input = gets.chomp
+    if user_input == "Y"
+      require_relative 'mail_content'
+    elsif
+      puts "Closing the program"
+
+    end
   elsif
     puts "Closing the program"
   end
