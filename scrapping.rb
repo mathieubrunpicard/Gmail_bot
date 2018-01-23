cac2e3696631c7693b75ce8bdb38304e1b7a3ba4
Mathieu Brun-Picard <b00673732@essec.edu>
2018-01-23 18:00:35 +0100

    Created an array for the valid input

--- scrapping.rb
+++ scrapping.rb
@@ -125,11 +125,12 @@ module Scrapping_module
 
         #Ask the user for a  department number
         puts "What is the department number you want to scrap ? [01-95 or 971-4]"
+        department_array = ["01","02","03","04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39", "40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","63","64","65","66","67","68","69","70","71","72","73","74","75","76","77","78","79","80","81","82","83","84","85","86","87","88","89","90","91","92","93","94","95","971","972","973","974"]
         user_number = gets.chomp
     #create an instance
     scrapper = Scrapping_module::Scrapping.new
     #Launch the script unless there is no input
-    unless user_number.empty?
+    if department_array.include? user_number
       #Launch the first method : get_all_the°urls_of_France
       #This script get all the urls of one department
       #And return an array of links
@@ -147,7 +148,9 @@ module Scrapping_module
       write_json(my_data_hash)
       return my_data_hash
     else
-      puts "So you don't want to scrap. Bye"
+      puts "Invalid input"
+      puts "So you don't want to scrap."
+      puts "Bye"
     end
   end
 
